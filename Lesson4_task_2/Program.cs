ffa131e8b458d0b589455d941fcea6fd2bc5c512
kot2d <kot2d@outlook.com>
2022-07-12 12:59:30 +0300

    added task 1 and 2 for lesson 4

---
+++ Lesson4_task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿int arraySize = 8;
+
+int[] generateArray(int arraySize){
+    Console.WriteLine("Генерация массива...");
+    int[] numbers = new int[arraySize];
+    for(int i=0; i<arraySize; i++){
+        numbers[i]= new Random().Next(1, arraySize);
+    }
+    Console.WriteLine("Генерация массива закончена");
+    return numbers;
+}
+
+void printArray(int[] array){
+    Console.WriteLine("Печать массива: ");
+    for(int i=0; i<array.Length; i++){
+        Console.WriteLine(array[i]);
+    }
+}
+
+int[] numbers = generateArray(8);
+printArray(numbers);
+
+
