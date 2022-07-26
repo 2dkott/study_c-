4d34e5a3392d9a0a817965dfadfcfed6ce38795f
kot2d <kot2d@outlook.com>
2022-07-26 21:38:42 +0300

    added lessona

---
+++ Lesson6_task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int getNumbersAndPringBelowZero(int size) {
+    
+    int counter=0;
+    
+    for (int i = 0; i < size; i++)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number<0) counter++;
+    }
+    return counter;
+}
+
+Console.WriteLine("Кол-во отрицательных чисел: " + getNumbersAndPringBelowZero(5)); 
---
+++ Lesson6_task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿bool findCrossPoint(double b1, double k1, double b2, double k2, double range)
+{
+    Console.WriteLine("Старт вычисления по формуле 'x * (k1 - k2) == b2 - b1' ...");
+    for (double i = -range; i <= range; i = Math.Round(i + .1f, 2)) {
+        if (i * (k1 - k2) == b2 - b1) {
+            double y = calculateYForLine(b1, k1, i);
+            Console.WriteLine("Вычисление завершено. Координаты точки пересечения=(" + i + "," + y + ")");
+            return true;
+        }
+    }
+    Console.WriteLine("Вычисление завершено. Точка пересечения не найдена!");
+    return false;
+}
+
+double calculateYForLine(double b, double k, double x) {
+    return k * x + b;
+}
+
+findCrossPoint(2f, 5f, 4f, 9f, 10f);
+
